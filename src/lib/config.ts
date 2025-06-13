316b9653beb28a7250108abbc1642d6058335d86
Izaak Meckler <ihmeckler@gmail.coom>
2025-06-13 12:11:03 -0400

    config code

---
+++ src/lib/config.ts
@@ -0,0 +1,38 @@
+import fs from "fs";
+import toml from "toml";
+import { exitWithError } from "./process";
+
+export interface TwilioConfig {
+    account_sid: string;
+    auth_token: string;
+    verify_service_sid: string;
+    messaging_service_sid: string;
+    phone_number: string;
+}
+
+export interface AppConfig {
+    general: {
+        domain: string;
+        port: string;
+        email: string;
+        site_name: string;
+    };
+    database: {
+        postgresql_url: string;
+    };
+    twilio?: TwilioConfig;
+}
+
+export const getConfig = (): AppConfig => {
+    try {
+        const config = toml.parse(
+            fs.readFileSync("./config/config.toml", "utf-8"),
+        ) as AppConfig;
+        return config
+    } catch {
+        exitWithError(
+            "Configuration file not found! Have you renamed './config/config-example.toml' to './config/config.toml'?",
+        );
+        return process.exit(1);
+    }
+};
